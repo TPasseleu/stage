6794714aabbeeaf34e74e70326d6b1709f581dae
Aymeric Delcampe <aymeric.delcampe@hotmail.fr>
2018-11-14 00:15:27 +0100

    accueil

---
+++ accueil/accueil.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="../css/app.css">
+	<link rel="stylesheet" type="text/css" href="../css/accueil.css">
+	<title>Bienvenue</title>
+</head>
+	<body>
+		<hr/>
+		<hr/>
+		<div class="container" style="border:1px solid #FDFD96;">
+		<div class="row">
+			<div class="col-xs-12">
+		
+			<p>Notre collectif vous invite à nous faire part de vos besoins et vos envies. Nous sommes tous habitant de la commune et voulons imaginer l’avenir de Cucq Trépied Stella. Agir ensemble sera plus efficace et constructif pour améliorer notre cadre de vie. Partageons ensemble nos idées.</p>
+		</div>
+		  </div> 
+		    </div> 
+	</body>
+</html>
