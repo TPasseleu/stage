281060e89d22300905cd6631a83afa28c36fae1f
Aymeric Delcampe <aymeric.delcampe@hotmail.fr>
2018-11-14 14:17:54 +0100

    bootstrap last version update

--- accueil/accueil.php
+++ accueil/accueil.php
@@ -2,12 +2,51 @@
 <html lang="fr">
 <head>
 	<meta charset="UTF-8">
-	<link rel="stylesheet" type="text/css" href="../css/app.css">
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="../css/accueil.css">
 	<title>Bienvenue</title>
 </head>
 	<body>
 		<hr/>
+   <nav class="navbar navbar-expand-lg navbar-light bg-faded">
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-item nav-link active" href="#">Accueil<span class="sr-only">(current)</span></a>
+              <a class="nav-item nav-link" href="#">Territoire</a>
+              <a class="nav-item nav-link" href="#">Blog</a>
+              <a class="nav-item nav-link disabled" href="#">Contact</a>
+            </div>
+          </div>
+   </nav>
+<!--		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		  <a class="navbar-brand" href="#">Navbar</a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+		  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+		    <ul class="navbar-nav">
+		      <li class="nav-item active">
+		        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Features</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Pricing</a>
+		      </li>
+		      <li class="nav-item dropdown">
+		        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		          Dropdown link
+		        </a>
+		        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+		          <a class="dropdown-item" href="#">Action</a>
+		          <a class="dropdown-item" href="#">Another action</a>
+		          <a class="dropdown-item" href="#">Something else here</a>
+		        </div>
+		      </li>
+		    </ul>
+		  </div>
+		</nav> -->
 		<hr/>
 		<div class="container" style="border:1px solid #FDFD96;">
 			<div class="row">
