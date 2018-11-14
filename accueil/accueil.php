7fc93b367a185f2c74d2bc8fb9a4b079d67a7c1b
Aymeric Delcampe <aymeric.delcampe@hotmail.fr>
2018-11-14 13:23:09 +0100

    modif accueil + add img

--- accueil/accueil.php
+++ accueil/accueil.php
@@ -10,12 +10,18 @@
 		<hr/>
 		<hr/>
 		<div class="container" style="border:1px solid #FDFD96;">
-		<div class="row">
-			<div class="col-xs-12">
-		
-			<p>Notre collectif vous invite à nous faire part de vos besoins et vos envies. Nous sommes tous habitant de la commune et voulons imaginer l’avenir de Cucq Trépied Stella. Agir ensemble sera plus efficace et constructif pour améliorer notre cadre de vie. Partageons ensemble nos idées.</p>
+			<div class="row">
+				<div class="col-md-12">
+				<p>Notre collectif vous invite à nous faire part de vos besoins et vos envies. Nous sommes tous habitant de la commune et voulons imaginer l’avenir de Cucq Trépied Stella. Agir ensemble sera plus efficace et constructif pour améliorer notre cadre de vie. Partageons ensemble nos idées.</p>
+				</div>
+			</div> 
 		</div>
-		  </div> 
-		    </div> 
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4"><img src="../img/img1.jpg"></div>
+				<div class="col-md-4"><img src="../img/img1.jpg"></div>
+				<div class="col-md-4"><img src="../img/img1.jpg"></div>
+			</div>
+		</div>
 	</body>
 </html>
