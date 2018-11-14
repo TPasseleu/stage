a8916b48baee2b80a639a9c2cdff601125f56355
Aymeric Delcampe <aymeric.delcampe@hotmail.fr>
2018-11-14 16:42:48 +0100

    navbar wip

--- accueil/accueil.php
+++ accueil/accueil.php
@@ -8,46 +8,17 @@
 </head>
 	<body>
 		<hr/>
-   <nav class="navbar navbar-expand-lg navbar-light bg-faded">
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-              <a class="nav-item nav-link active" href="#">Accueil<span class="sr-only">(current)</span></a>
-              <a class="nav-item nav-link" href="#">Territoire</a>
-              <a class="nav-item nav-link" href="#">Blog</a>
-              <a class="nav-item nav-link disabled" href="#">Contact</a>
-            </div>
-          </div>
-   </nav>
-<!--		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-		  <a class="navbar-brand" href="#">Navbar</a>
-		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-		    <span class="navbar-toggler-icon"></span>
-		  </button>
-		  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-		    <ul class="navbar-nav">
-		      <li class="nav-item active">
-		        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-		      </li>
-		      <li class="nav-item">
-		        <a class="nav-link" href="#">Features</a>
-		      </li>
-		      <li class="nav-item">
-		        <a class="nav-link" href="#">Pricing</a>
-		      </li>
-		      <li class="nav-item dropdown">
-		        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-		          Dropdown link
-		        </a>
-		        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-		          <a class="dropdown-item" href="#">Action</a>
-		          <a class="dropdown-item" href="#">Another action</a>
-		          <a class="dropdown-item" href="#">Something else here</a>
+		   <nav class="navbar navbar-expand-lg navbar-light bg-faded">
+		        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+		        	<div class="navbar-nav">
+		              <a class="nav-item nav-link active" href="#">Accueil<span class="sr-only">(current)</span></a>
+		              <a class="nav-item nav-link" href="#">Territoire</a>
+		              <a class="nav-item nav-link" href="#">Blog</a>
+		              <a class="nav-item nav-link disabled" href="#">Contact</a>
+		            </div>
 		        </div>
-		      </li>
-		    </ul>
-		  </div>
-		</nav> -->
-		<hr/>
+		   </nav>
+		<hr id="hr2">
 		<div class="container" style="border:1px solid #FDFD96;">
 			<div class="row">
 				<div class="col-md-12">
@@ -55,11 +26,12 @@
 				</div>
 			</div> 
 		</div>
-		<div class="container">
-			<div class="row">
+		<div id="space"></div>
+		<div class="container-fluid">
+			<div class="row" id="cont">
 				<div class="col-md-4"><img src="../img/img1.jpg"></div>
 				<div class="col-md-4"><img src="../img/img1.jpg"></div>
-				<div class="col-md-4"><img src="../img/img1.jpg"></div>
+				<div class="col-md-4" id="force"><img src="../img/img1.jpg"></div>
 			</div>
 		</div>
 	</body>
