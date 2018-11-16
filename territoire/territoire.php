129d6f10500857e3451e8a4e917c1dde9b35d44b
Aymeric Delcampe <aymeric.delcampe@hotmail.fr>
2018-11-16 09:27:09 +0100

    territoire

---
+++ territoire/territoire.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../css/territoire.css">
+	<title>Territoire</title>
+</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 container" style="border:1px solid #FDFD96;">
+					<h2>CTS</h2>
+					<h3>Aménagement Territoire</h3>
+					<h3>Association</h3>
+					<h5>Quartier</h5>
+					<h5>Scolaire</h5>
+						<div class="dropdown">
+						  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						    Dropdown
+						  </button>
+						  <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+						    <button class="dropdown-item" type="button">Action</button>
+						    <button class="dropdown-item" type="button">Another action</button>
+						    <button class="dropdown-item" type="button">Something else here</button>
+						  </div>
+						</div>	
+				</div>
+				<div class="col-md-8">
+					<h2>Test</h2>	
+				</div>				
+			</div>
+		</div>
+	</body>
+</html>
