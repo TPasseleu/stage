d10c2fe687457e9531128c8fd66f2137b1c49ace
Aymeric Delcampe <aymeric.delcampe@hotmail.fr>
2018-11-16 12:03:11 +0100

    territoire update1

--- territoire/territoire.php
+++ territoire/territoire.php
@@ -4,32 +4,78 @@
 	<meta charset="UTF-8">
 	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="../css/territoire.css">
+	<script type="text/javascript" src="../js/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="../js/bootstrap.bundle.min.js"></script>
+
 	<title>Territoire</title>
 </head>
 	<body>
+		<?php require "../inc/navbar.php"; ?>
 		<div class="container">
 			<div class="row">
 				<div class="col-md-4 container" style="border:1px solid #FDFD96;">
 					<h2>CTS</h2>
 					<h3>Aménagement Territoire</h3>
-					<h3>Association</h3>
-					<h5>Quartier</h5>
-					<h5>Scolaire</h5>
-						<div class="dropdown">
-						  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						    Dropdown
-						  </button>
-						  <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-						    <button class="dropdown-item" type="button">Action</button>
-						    <button class="dropdown-item" type="button">Another action</button>
-						    <button class="dropdown-item" type="button">Something else here</button>
-						  </div>
-						</div>	
+					<p>
+					  <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+					   Association
+					  </button>
+					</p>
+					<div class="collapse" id="collapseExample">
+					  <div class="card card-body">
+					    Scolaire
+					  </div>
+					<div class="card card-body">
+					    Quartier
+					  </div>
+					</div>
 				</div>
 				<div class="col-md-8">
 					<h2>Test</h2>	
 				</div>				
 			</div>
 		</div>
+<!--test-->
+<hr>
+<!--
+      <nav class="navbar navbar-light bg-danger">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class=" fab fa-medium-m"></span>
+    </button>
+        </nav>
+    -->
+   <!-- 
+<div class="pos-f-t">
+        <div class="collapse" id="navbarToggleExternalContent">
+            <div class="bg-grey p-4">
+                <div class="btn-group-vertical" role="group" aria-label="Button group with nested dropdown" class="btn-group-vertical">
+                <a href="../testcode/testcode.html" type="button" class="btn btn-danger"><i class="fas fa-file-signature"></i> Test code</a>
+                    <a href="../contact/contact.html" type="button" class="btn btn-white"><i class="fas fa-phone-square"></i>      contact</a>
+                    <div class="btn-group" role="group">
+                        <button id="btnGroupDrop1" type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-address-book"></i>
+                                         Annuaire
+                                                  </button>
+                        <div class="dropdown-menu"  aria-labelledby="btnGroupDrop1">
+                            <a class="btn btn-info" href="../annuaire/annuaire.html">Berck</a>
+                            <a class="btn btn-white" href="../annuaire/montreuil.html">Montreuil</a>
+                            <a class="btn btn-danger" href="../annuaire/etaple.html">Etaples</a>
+                            <a class="btn btn-dark"  href="../annuaire/autre.html">Autres</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+</div>
+
+<hr>
+
+<div class="pos-f-t">
+        <div class="collapse" id="navbarToggleExternalContent">
+            <div class="bg-grey p-4">
+                <div class="btn-group-vertical" role="group" aria-label="Button group with nested dropdown" class="btn-group-vertical">
+                <a href="../testcode/testcode.html" type="button" class="btn btn-danger"><i class="fas fa-file-signature"></i> Test code</a>
+</div></div></div></div> -->
+<hr>
+    
 	</body>
 </html>
