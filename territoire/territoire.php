52b16f24af2f6c320feea53c4ff10e438349f479
Aymeric Delcampe <aymeric.delcampe@hotmail.fr>
2018-11-20 15:45:43 +0100

    territoire update 2

--- territoire/territoire.php
+++ territoire/territoire.php
@@ -13,68 +13,42 @@
 		<?php require "../inc/navbar.php"; ?>
 		<div class="container">
 			<div class="row">
-				<div class="col-md-4 container" style="border:1px solid #FDFD96;">
+				<div class="col-md-4 container text-center" style="border:1px solid #FDFD96;">
 					<h2>CTS</h2>
 					<h3>Aménagement Territoire</h3>
-					<p>
-					  <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+					<p>			
+					    <button class="btn btn-primary btn-block" type="button" id="amenagement">Aménagement de territoire</button>		
+					    <button class="btn btn-primary btn-block" type="button" id="probleme">Problème de commune</button>	
+					    <button class="btn btn-primary btn-block" type="button" id="permanence">Permanence mairie</button>
+					    <button class="btn btn-primary btn-block" type="button" id="poubelle">Ramassage poubelle</button>	
+					<button class="btn btn-primary btn-block" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
 					   Association
-					  </button>
+					</button>
 					</p>
-					<div class="collapse" id="collapseExample">
-					  <div class="card card-body">
-					    Scolaire
-					  </div>
-					<div class="card card-body">
-					    Quartier
-					  </div>
+					<div class="collapse" id="collapseExample">				  
+					    <button class="btn btn-primary" type="button" id="scolaire">Scolaire</button><br/>
+						<button class="btn btn-primary" type="button" id="quartier">Quartier</button><br/>  
+					    <button class="btn btn-primary" type="button" id="sportive">Sportive</button><br/>	
 					</div>
 				</div>
 				<div class="col-md-8">
-					<h2>Test</h2>	
+					<h2>Test</h2>
+					<div id="div1"><h2>Rien</h2></div>		
 				</div>				
 			</div>
 		</div>
-<!--test-->
-<hr>
-<!--
-      <nav class="navbar navbar-light bg-danger">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class=" fab fa-medium-m"></span>
-    </button>
-        </nav>
-    -->
-   <!-- 
-<div class="pos-f-t">
-        <div class="collapse" id="navbarToggleExternalContent">
-            <div class="bg-grey p-4">
-                <div class="btn-group-vertical" role="group" aria-label="Button group with nested dropdown" class="btn-group-vertical">
-                <a href="../testcode/testcode.html" type="button" class="btn btn-danger"><i class="fas fa-file-signature"></i> Test code</a>
-                    <a href="../contact/contact.html" type="button" class="btn btn-white"><i class="fas fa-phone-square"></i>      contact</a>
-                    <div class="btn-group" role="group">
-                        <button id="btnGroupDrop1" type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-address-book"></i>
-                                         Annuaire
-                                                  </button>
-                        <div class="dropdown-menu"  aria-labelledby="btnGroupDrop1">
-                            <a class="btn btn-info" href="../annuaire/annuaire.html">Berck</a>
-                            <a class="btn btn-white" href="../annuaire/montreuil.html">Montreuil</a>
-                            <a class="btn btn-danger" href="../annuaire/etaple.html">Etaples</a>
-                            <a class="btn btn-dark"  href="../annuaire/autre.html">Autres</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-    </div>
-</div>
+				<script type="text/javascript">
+						$(function () {$("#amenagement").click(function(){$.ajax({url:"amenagement.html",success:function(result){$("#div1").html(result);}});});});
+						$(function () {$("#probleme").click(function(){$.ajax({url:"probleme.html",success:function(result){$("#div1").html(result);}});});});
+						$(function () {$("#permanence").click(function(){$.ajax({url:"permanence.html",success:function(result){$("#div1").html(result);}});});});
+						$(function () {$("#poubelle").click(function(){$.ajax({url:"poubelle.html",success:function(result){$("#div1").html(result);}});});});
+					    $(function () {$("#scolaire").click(function(){$.ajax({url:"scolaire.html",success:function(result){$("#div1").html(result);}}); });});				  					
+					    $(function () {$("#quartier").click(function(){$.ajax({url:"quartier.html",success:function(result){$("#div1").html(result);}});});});				  
+						$(function () {$("#sportive").click(function(){$.ajax({url:"sportive.html",success:function(result){$("#div1").html(result);}});});})	;
+					</script>
+
 
 <hr>
-
-<div class="pos-f-t">
-        <div class="collapse" id="navbarToggleExternalContent">
-            <div class="bg-grey p-4">
-                <div class="btn-group-vertical" role="group" aria-label="Button group with nested dropdown" class="btn-group-vertical">
-                <a href="../testcode/testcode.html" type="button" class="btn btn-danger"><i class="fas fa-file-signature"></i> Test code</a>
-</div></div></div></div> -->
 <hr>
     
 	</body>
