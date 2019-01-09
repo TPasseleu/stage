8a080e5a987b73fb65a2a999287caf7e1a59e61a
Unknown <bigot.catherine@club-internet.fr>
2019-01-09 16:47:02 +0100

    Correction et ajour actu
    Cor contact

--- blog.php
+++ blog.php
@@ -2,15 +2,75 @@
 include './include/header.php';
 include './include/navbar.php';
 ?>
-
+<style>
+    article h3{
+        text-align: center;
+    }
+    article{
+        position: relative;
+        display: block;
+        text-align: center;
+        padding: 15px;
+        background-color: antiquewhite;
+        border-radius: 5px 5px 5px 5px;
+        box-shadow: 5px 5px 15px black;
+        left: 15rem;
+    }
+    .comment {
+    display: block;
+    margin-left: 20rem;
+}
+    article p{
+        text-align: justify;
+    }
+</style>
     <div class="page">
-        <p>Le lien vers notre page <a target="_blank" href="https://www.facebook.com/GTN-1585686084872136/">Facebook</a></p>
+        
     </div>
     <main role="main" class="container">
-        <br>
+       <article><h3>Internet et TV 
+           
+                              La galère !</h3>
 
-        <p class="commentaire">Poster vos commentaires ici</p>
-        <div class="fb-comments" data-href="http://127.0.0.1:49317/presentationex.html" data-width="500" data-numposts="5"></div>
+ <br>
+
+<p>Nous sommes depuis des jours, des mois, des années incapables de recevoir la télévision sans coupures, elles peuvent durer de quelques minutes, à plusieurs heures, voire des jours.
+    
+    Concernant Internet, une partie de Cucq et Stella ont un débit inférieur à 3Mbps. Je tiens simplement  à rappeler que pour pouvoir télécharger des films, des photos, des dossiers et recevoir la télévision par l’adsl , un minimum de 10 Mbps est souhaité.
+    
+     Donc dans les deux cas, je me pose cette question :
+    
+           -Sommes-nous les dindons de la farce ?
+    
+    Il faut avouer qu’après les fêtes de fin d’année, cette phrase peut prêter à sourire !
+    
+    Mais pour les personnes concernées par l’un ou l’autre, parfois même par les deux problèmes, cela n’a rien de risible. Les deux sujets sont importants dans notre vie quotidienne et indispensables pour avoir une vue sur le monde 
+    
+    Des solutions nous sont proposées mais sont-elles à la hauteur de nos attentes ?
+    
+    -pour la télévision : une aide financière qui était mise en place sur le Touquet, Etaples, Camiers  vient d’être étendue aux communes de Merlimont, Berck, Rang du Fliers et Cucq jusqu’au 29 mars 2019.
+    
+    Pour toute les démarches un numéro non surtaxé
+    
+    09 70 818 818  et un site Internet sont  à disposition : recevoir la tnt.fr.
+    
+    -  Pour l’installation d’une parabole
+    
+                    ° 250 euros pour un particulier
+    
+                    ° 500  euros pour un gestionnaire d’immeuble
+    
+    Elle est accordée sans condition de ressources, la pose doit être effectuée par un antenniste avec facture à l’appui. Attention cela ne concerne que les résidences principales.
+    
+    Pour les autres, eh bien tant pis !
+    
+    Comme c’est ballot alors que notre commune  compte</p> </article>
+        <br>
+       <div class="comment">
+             <p>Le lien vers notre page <a target="_blank" href="https://www.facebook.com/GTN-1585686084872136/">Facebook</a></p>
+            <p class="commentaire">Poster vos commentaires ici</p>
+            <div class="fb-comments" data-href="http://127.0.0.1:49317/presentationex.html" data-width="500" data-numposts="5"></div>
+        </div>
 
         
         
