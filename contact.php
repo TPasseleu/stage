6c775e58c05e604914cb3132eaf0a5f9d8cfa83b
Unknown <bigot.catherine@club-internet.fr>
2019-01-09 16:47:02 +0100

    Correction et ajour actu
    Cor contact

--- contact.php
+++ contact.php
@@ -19,16 +19,16 @@ include './include/navbar.php';
                 <h4 class="card-title">Christelle Deharbe</h4>
                 <h5 class="card-title">Dirigente de l'association</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Christelle.Deharbe@orange.fr</a>
+                <a href="mailto:letrianglecitoyen@gmail.com">letrianglecitoyen@gmail.com</a>
             </div>
         </div>
         <div class="card m-3" style="width: 22rem;">
-            <img class="card-img-top" src="img/bigot.JPG" alt="Card image cap">
+            <img class="card-img-top" src="img/Catherine.JPG" alt="Card image cap">
             <div class="card-body">
                 <h4 class="card-title">Catherine Bigot</h4>
                 <h5 class="card-title">Dirigente de l'association</h5>
                 <p class="card-text">Présidente de l'association de quartier " Le Parc Soleil" Trépied Cucq.</p>
-                <a href="#" class="btn btn-primary">Bigot.Catherine@club-internet.fr</a>
+                <a href="mailto:letrianglecitoyen@gmail.com">letrianglecitoyen@gmail.com</a>
             </div>
         </div>
         <div class="card m-3" style="width: 22rem;">
@@ -37,7 +37,7 @@ include './include/navbar.php';
                 <h4 class="card-title">Bruno Semain</h4>
                 <h5 class="card-title">Dirigent de l'association</h5>
                 <p class="card-text">Tresorier du GTNSM</p>
-                <a href="#" class="btn btn-primary">mail@example.com</a>
+                <a href="mailto:letrianglecitoyen@gmail.com">letrianglecitoyen@gmail.com</a>
             </div>
         </div>
         <div class="card m-3" style="width: 22rem;">
@@ -46,7 +46,7 @@ include './include/navbar.php';
                 <h4 class="card-title">Prénom Nom</h4>
                 <h5 class="card-title">Rôle</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">mail@example.com</a>
+                <a href="mailto:letrianglecitoyen@gmail.com">letrianglecitoyen@gmail.com</a>
             </div>
         </div>
     </div>
